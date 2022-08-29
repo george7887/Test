0f7ea7902daf5a318034ca11cf7eb88203130dc9
george7887 <george788719@gmail.com>
2022-08-29 15:29:53 +0300

    C# программа квадрат вводимого числа

--- Program.cs
+++ Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(n * n);

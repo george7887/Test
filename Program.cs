55c45943fd1959c9a02d79e5f5ed9581ade1954e
george7887 <george788719@gmail.com>
2022-08-29 16:47:03 +0300

    Написал программу день недели

--- Program.cs
+++ Program.cs
@@ -1,8 +1,22 @@
-﻿Console.Write("Введите первое число: "); 
+﻿Console.Write("Введите номер дня недели: "); 
 int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-if (n / m == m)
-    Console.WriteLine("yes");
-else
-    Console.WriteLine("no");
+
+while (n < 1 || n > 7)
+{
+    Console.Write("Вы ошиблись! Введите еще раз: ");
+    n = Convert.ToInt32(Console.ReadLine());
+}
+if (n == 1)
+    Console.WriteLine("Понедельник");
+if (n == 2)
+    Console.WriteLine("Вторник");
+if (n == 3)
+    Console.WriteLine("Среда");
+if (n == 4)
+    Console.WriteLine("Четверг");
+if (n == 5)
+    Console.WriteLine("Пятница");
+if (n == 6)
+    Console.WriteLine("Суббота");
+if (n == 7)
+    Console.WriteLine("Воскресенье");
